f4b50f858aa235b561e1ef401532bbf94ba51553
jakobbbb <jakobbbb@users.noreply.github.com>
2018-01-20 04:39:53 +0100

    greatly improve deploy script

--- deploy.py
+++ deploy.py
@@ -1,16 +1,29 @@
+#!/usr/bin/python
+
 import os
+import subprocess
+from sys import argv
 
-deploy = (".vim", ".config", ".zsh", ".mutt") # files with this prefix are symlinked
+deploy = (".vim", ".config", ".zsh", ".mutt", ".mozilla") # files with this prefix are symlinked
 
-HOMEDIR = "/home/jakob/"
-DOTFILEDIR = "/home/jakob/dotfiles/"
+HOMEDIR = os.path.expanduser("~") + "/"
+DOTFILEDIR = os.getcwd() + "/"
 
-def symlink(f1, f2):
-    print "ln -sr %s %s" % (f1, f2)
-    return
+no_errors = True
+
+def execute(cmd):
+    if len(argv) == 2 and argv[1] == "dry":
+        print " ".join(cmd)
+    else:
+        try:
+            subprocess.check_call(cmd)
+        except Exception, e:
+            no_errors = False
+            print(":( "+e)
 
 def getFFprofile():
-    # figure out default ff profile
+    # Read FF's profiles.ini to find active FF profile
+    # (used for deploying userChrome.css)
     ini = {}
     try:
         with open(HOMEDIR+"/.mozilla/firefox/profiles.ini", "r") as f:
@@ -24,17 +37,40 @@ def getFFprofile():
                         ini[section][key] = value
                         if [key, value] == ["Default", "1"]:
                             default = section
+        return ini[default]["Path"]
     except Exception, e:
-        print "[:(]", e
-    return ini[default]["Path"]
+        print(":( (FF) "+e)
+
 
 for root, dirs, files in os.walk(".", topdown=True):
     for name in files:
-        file_name = os.path.join(root, name)
-        file_short = file_name[2:]
-        if file_short.startswith(deploy):
-            print "mkdir --parents " + HOMEDIR + root[2:]
-            symlink(DOTFILEDIR+file_short, HOMEDIR+file_short)
-        elif file_short.startswith(".mozilla"):
-            symlink(DOTFILEDIR+file_short, HOMEDIR+file_short.replace("profile", getFFprofile()))
+        file_name = os.path.join(root, name) # starts with ./
+        file_short = file_name[2:] # ./ removed
+        root_short = root[2:] # ./ removed
 
+        do_deploy = True # assume file is to be symlinked
+
+        if file_short.startswith(deploy): # in whitelist?
+
+            if file_short.startswith(".mozilla"): # special FF target
+                FFprofile = getFFprofile()
+                if FFprofile:
+                    target = HOMEDIR+file_short.replace("profile", FFprofile)
+            else: # default target
+                target = HOMEDIR+file_short
+
+            if os.path.exists(target): # target file exists?
+                if os.path.realpath(target) == DOTFILEDIR+file_short: # is already properly symlinked?
+                    do_deploy = False
+                else:
+                    execute(["mv", target, target+".bak"])
+            elif not os.path.exists(root_short): # parent folder doesn't exist?
+               execute(["mkdir", "--parents", HOMEDIR+root_short])
+
+            if do_deploy:
+                execute(["ln", "-sr", DOTFILEDIR+file_short, target])
+
+if no_errors:
+    print(":) Sucessfully deployed dotfiles!")
+else:
+    print(":/ Some things went wrong")
