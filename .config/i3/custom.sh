9090dd9fd4c9c036d55b6de00afec4b53eb50f69
jakobbbb <jakobbbb@users.noreply.github.com>
2018-05-01 14:14:39 +0200

    dual monitor config

--- .config/i3/custom.sh
+++ .config/i3/custom.sh
@@ -5,3 +5,10 @@ setxkbmap -option caps:none
 
 # start dropbox
 dropbox start &
+
+# xrandr config
+if [ "$HOSTNAME" = jbr-ubuntu ]; then
+    xrandr --output DVI-D-1 --primary
+    xrandr --output DVI-D-1 --left-of DVI-I-1
+    xrandr --output DVI-D-1 --mode 1920x1080 --rate 144
+fi
