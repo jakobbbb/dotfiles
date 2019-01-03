4bb19674dd2a03d804cf66a12283a177769e82a5
jakobbbb <jakobbbb@users.noreply.github.com>
2019-01-03 13:02:11 +0100

    move some of i3's custom.sh into custom.d (ignored by git)

--- .config/i3/custom.sh
+++ .config/i3/custom.sh
@@ -6,9 +6,8 @@ setxkbmap -option caps:none
 # start dropbox
 dropbox start &
 
-# xrandr config
-if [ "$HOSTNAME" = jbr-ubuntu ]; then
-    xrandr --output DVI-D-1 --primary
-    xrandr --output DVI-D-1 --left-of DVI-I-1
-    xrandr --output DVI-D-1 --mode 1920x1080 --rate 144
-fi
+# execute files in custom.d
+for f in ~/.config/i3/custom.d/*
+do
+    sh $f
+done
