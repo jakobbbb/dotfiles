325a834fb2271d66f769fe32b189c3ec5ee6fc8d
Jakob Ruckel <jakob@ruckel.de>
2019-11-03 23:42:57 +0100

    i3: show notification for volume changes
    Only really needed in fullscreen, but quite useful there.
    Also increase alert level for volumes above 100% and brightness of 0% or
    100%.

--- .config/i3/scripts/brightness-notification.sh
+++ .config/i3/scripts/brightness-notification.sh
@@ -3,7 +3,9 @@
 # Show the current xbacklight brightness in a notification.
 TIMEOUT=600
 ID=2527269
+BRIGHTNESS=$(echo "$(xbacklight -get)/1" | bc)
 PRIO="low"
-MESSAGE="Backlight: $(echo "$(xbacklight -get)/1" | bc)%"
+([ "$BRIGHTNESS" -eq "0" ] || [ "$BRIGHTNESS" -eq "100" ]) && PRIO="normal"
+MESSAGE="Backlight: $BRIGHTNESS%"
 
 dunstify -t $TIMEOUT -r $ID -u $PRIO "$MESSAGE"
