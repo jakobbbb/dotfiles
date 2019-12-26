dc99a29f93e47d16fdabfd630035125d820aa318
Jakob Ruckel <jakob@ruckel.de>
2019-12-26 16:35:03 +0100

    i3: add script to choose window to focus

---
+++ .config/i3/scripts/choose-focus.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+import i3ipc
+import subprocess
+
+
+def get_name(window):
+    return "%s - %s" % (window.window_class, window.window_title)
+
+
+def dmenu_choose(items):
+    """
+    Get a choice using dmenu.
+    """
+    items = "\n".join(sorted(items))
+    cmd = ["dmenu", "-l", "30", "-i"]
+    p = subprocess.Popen(
+        cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+    )
+    out = p.communicate(input=items.encode())[0]
+    return out.decode().strip()
+
+
+def choose_window(windows):
+    """
+    Let the user choose a window via dmenu.
+    """
+    entries = {}  # unique name -> window
+    fmt = "%s #%s"
+    for w in windows:
+        if get_name(w) in entries.keys():
+            # Update the other entry
+            entries[fmt % (get_name(w), entries.pop(get_name(w)).id)] = entries[
+                get_name(w)
+            ]
+            name = fmt % (get_name(w), w.id)
+        else:
+            name = get_name(w)
+        entries[name] = w
+    choice = dmenu_choose(entries.keys())
+    return entries[choice]
+
+
+def get_windows(ws):
+    """
+    Get a list of all windows in a workspace.
+    """
+    windows = []
+    for w in ws.descendants():
+        if w.window:
+            windows.append(w)
+    return windows
+
+
+def main():
+    i3 = i3ipc.Connection()
+    ws = i3.get_tree().find_focused().workspace()
+
+    wins = get_windows(ws)
+    if len(wins) == 0:
+        return
+
+    focus = choose_window(wins)
+    focus.command("focus")
+
+
+if __name__ == "__main__":
+    main()
