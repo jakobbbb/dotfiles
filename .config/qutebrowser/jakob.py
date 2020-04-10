d7c2153c40fb215a2d1bd9bebddbfddfb857c3af
Jakob Ruckel <jakob@ruckel.de>
2020-04-11 00:10:18 +0200

    qutebrowser: clean up config

--- .config/qutebrowser/jakob.py
+++ .config/qutebrowser/jakob.py
@@ -1,80 +1,31 @@
-# My base configuration.
+# Base configuration and color scheme.
 # Add the following line to the main `config.py`"
 #   config.source('jakob.py')
 
-# Enable JavaScript.
-# Type: Bool
+# Disable JS
 c.content.javascript.enabled = False
+# Exceptions
+for url in ['chrome-devtools://*', 'devtools://*', 'chrome://*/*', 'qute://*/*']:
+    config.set('content.javascript.enabled', True, url)
 
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'chrome-devtools://*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'devtools://*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'chrome://*/*')
-
-# Enable JavaScript.
-# Type: Bool
-config.set('content.javascript.enabled', True, 'qute://*/*')
-
-# Allow websites to show notifications.
-# Type: BoolAsk
-# Valid values:
-#   - true
-#   - false
-#   - ask
+# No notifications at all
 config.set('content.notifications', False)
 
 # Open new tabs (middleclick/ctrl+click) in the background.
-# Type: Bool
 c.tabs.background = True
 
 # Stack related tabs on top of each other when opened consecutively.
-# Only applies for `next` and `prev` values of
-# `tabs.new_position.related` and `tabs.new_position.unrelated`.
-# Type: Bool
 c.tabs.new_position.stacking = True
 
-# Position of the tab bar.
-# Type: Position
-# Valid values:
-#   - top
-#   - bottom
-#   - left
-#   - right
+# Tab bar.
 c.tabs.position = 'left'
-
-# Width (in pixels or as percentage of the window) of the tab bar if
-# it's vertical.
-# Type: PercOrInt
 c.tabs.width = '13%'
-
-# Background color of the tab bar.
-# Type: QssColor
 c.colors.tabs.bar.bg = '#0f131f'
-
-# Background color of unselected odd tabs.
-# Type: QtColor
 c.colors.tabs.odd.bg = '#333744'
-
-# Background color of unselected even tabs.
-# Type: QtColor
 c.colors.tabs.even.bg = '#4c505e'
-
-# Background color of selected odd tabs.
-# Type: QtColor
 c.colors.tabs.selected.odd.bg = '#405779'
-
-# Background color of selected even tabs.
-# Type: QtColor
 c.colors.tabs.selected.even.bg = '#405779'
 
-# Bindings for normal mode
 # Bindings for normal mode
 config.bind(',M', 'hint links spawn mpv {hint-url}')
 config.bind(',P', 'hint links run open -p {hint-url}')
