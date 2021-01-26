844fece2999dd269610969b39c7842480a034b82
Jakob Ruckel <jakob@ruckel.de>
2021-01-26 04:22:58 +0100

    Add settings for default applications

---
+++ misc/xdg-settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Set a bunch of X settings.
+# These settings persist, so this script only needs to be run once.
+
+xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
+xdg-mime default org.pwmt.zathura.desktop application/pdf
