59355934e8339b14e059132c5fb3c3a2f4909983
Jakob Ruckel <jakob@ruckel.de>
2021-01-26 04:08:44 +0100

    i3/custom: Move keyboard cfg to appropriate file

--- .config/i3/custom.avail/005-keyboard.sh
+++ .config/i3/custom.avail/005-keyboard.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+
+# disable capslock
+setxkbmap -option caps:none
+
+# us international layout
 setxkbmap us altgr-intl
+
+# load xmodmap
 xmodmap ~/.config/xmodmap
--- .config/i3/custom.sh
+++ .config/i3/custom.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# disable capslock
-setxkbmap -option caps:none
-
 # execute files in custom.d
 for f in ~/.config/i3/custom.d/*.sh
 do
