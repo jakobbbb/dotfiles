db0be2f650e0363dcc1057590aad30bf0811f358
Jakob Ruckel <jakob@ruckel.de>
2021-04-30 16:05:40 +0200

    Fix redshift for tablet

--- .config/i3/custom.avail/010-redshift.sh
+++ .config/i3/custom.avail/010-redshift.sh
@@ -2,5 +2,5 @@
 
 redshift -PO 3400
 
-TABLET=`xrandr --listactivemonitors | grep HDMI-0 | cut -d: -f1`
+TABLET=`xrandr --listactivemonitors | grep HDMI-0 | cut -d: -f1 | sed -e 's/ //g'`
 redshift -m randr:crtc=$TABLET -x
