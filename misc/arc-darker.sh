b00c45a0ef9b9f1c9a5305e99b7fbb5e1f46880f
Jakob Ruckel <jakob@ruckel.de>
2021-09-10 01:33:26 +0200

    Replace arc theme patch with script

---
+++ misc/arc-darker.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+function make_darker() {
+    echo "Making theme darker..."
+
+    SUBS=(
+      "s/#404552/#2e3341/gi"
+      "s/#d3dae3/#d9dfe7/gi"
+      "s/#383c4a/#272a38/gi"
+      "s/#d3dae3/#d9dfe7/gi"
+      "s/#4b5162/#393f53/gi"
+      "s/#3e4350/#2c313e/gi"
+      "s/#7c818c/#737a88/gi"
+      "s/#404552/#2e3341/gi"
+      "s/#353945/#242833/gi"
+      "s/#5294e2/#4192e6/gi"
+      "s/#383c4a/#272a38/gi"
+      "s/#444a58/#363b49/gi"
+      "s/#505666/#414757/gi"
+    )
+
+    for s in ${SUBS[@]}; do
+        find . -type f -exec sed -i "$s" {} \;
+    done
+}
