222e7c39b3fceecce92314e337924f571739d2d4
Jakob Ruckel <jakob@ruckel.de>
2021-09-16 12:58:21 +0200

    Update xrandr-argon display names

--- .config/i3/custom.avail/000-xrandr-argon.sh
+++ .config/i3/custom.avail/000-xrandr-argon.sh
@@ -4,20 +4,20 @@ TABLET_STYLUS=`xinput | grep "Tablet Monitor stylus" | sed -e "s/.*id=\(..\).*/\
 TABLET=`xinput | grep 'Tablet Monitor Pad pad' | sed -e "s/.*id=\(..\).*/\1/g"`
 
 #xrandr --output DVI-D-0 --off
-xrandr --output LVDS1 --auto
+xrandr --output LVDS-1 --auto
 
 if [ "$1" = "tablet" ]; then
-    xrandr --output HDMI1 --auto --$2 LVDS1
+    xrandr --output HDMI-1 --auto --$2 LVDS-1
 elif [ "$1" = "tabletmirror" ]; then
-    xrandr --output HDMI1 --auto --same-as $2
+    xrandr --output HDMI-1 --auto --same-as $2
 elif [ "$1" = "notablet" ]; then
-    xrandr --output HDMI1 --off
+    xrandr --output HDMI-1 --off
 fi
 
 if [[ $1 = tablet* ]]; then
-    xrandr --output HDMI1 --rotate inverted
+    xrandr --output HDMI-1 --rotate inverted
     xrandr --dpi 96
-    xinput map-to-output $TABLET_STYLUS HDMI1
+    xinput map-to-output $TABLET_STYLUS HDMI-1
     xinput set-prop $TABLET_STYLUS "Wacom Tablet Area" -89, 193, 58641, 32793
 fi
 
