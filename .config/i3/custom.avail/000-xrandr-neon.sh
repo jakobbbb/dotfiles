158720a216e07c4bd8b369ccfaaa3db3cb81c632
Jakob Ruckel <jakob@ruckel.de>
2022-05-27 13:51:25 +0200

    i3/xrandr: Update screen names

--- .config/i3/custom.avail/000-xrandr-neon.sh
+++ .config/i3/custom.avail/000-xrandr-neon.sh
@@ -3,14 +3,14 @@
 TABLET_STYLUS=`xinput | grep "Tablet Monitor stylus" | sed -e "s/.*id=\(..\).*/\1/g"`
 TABLET=`xinput | grep 'Tablet Monitor Pad pad' | sed -e "s/.*id=\(..\).*/\1/g"`
 
-#xrandr --output DVI-D-0 --off
-xrandr --output DVI-D-0 --mode 1920x1080 -r 60
-xrandr --output DVI-I-1 --mode 1920x1080 -r 144
-xrandr --output DVI-I-1 --primary
-xrandr --output DVI-D-0 --auto --left-of DVI-I-1
+#xrandr --output DP-2 --off
+xrandr --output DP-2 --mode 1920x1080 -r 60
+xrandr --output DP-3 --mode 1920x1080 -r 144
+xrandr --output DP-3 --primary
+xrandr --output DP-2 --auto --left-of DP-3
 
 if [ "$1" = "tablet" ]; then
-    xrandr --output HDMI-0 --auto --below DVI-D-0
+    xrandr --output HDMI-0 --auto --below DP-2
 elif [ "$1" = "tabletmirror" ]; then
     xrandr --output HDMI-0 --auto --same-as $2
 elif [ "$1" = "notablet" ]; then
