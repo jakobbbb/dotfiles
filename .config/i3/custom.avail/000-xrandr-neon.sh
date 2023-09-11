2982b86fe206b239a46b356352e989675109372c
Jakob Ruckel <jakob@ruckel.de>
2023-09-11 20:34:22 +0200

    Re-calibrate tablet

--- .config/i3/custom.avail/000-xrandr-neon.sh
+++ .config/i3/custom.avail/000-xrandr-neon.sh
@@ -6,13 +6,13 @@ SCREEN_LEFT=DisplayPort-1
 SCREEN_RIGHT=DisplayPort-2
 SCREEN_TABLET=HDMI-A-0
 
-TABLET_STYLUS=`xinput | grep "Tablet Monitor stylus" | sed -e "s/.*id=\(..\).*/\1/g"`
+TABLET_STYLUS=`xinput | grep "Tablet Monitor Pen stylus" | sed -e "s/.*id=\(..\).*/\1/g"`
 TABLET=`xinput | grep 'Tablet Monitor Pad pad' | sed -e "s/.*id=\(..\).*/\1/g"`
 
 
 if [ "$1" = "tablet" ]; then
     xrandr --output $SCREEN_TABLET --auto
-    xrandr --output $SCREEN_TABLET --below $SCREEN_LEFT
+    xrandr --output $SCREEN_TABLET --below $SCREEN_RIGHT
 elif [ "$1" = "tabletmirror" ]; then
     xrandr --output $SCREEN_TABLET --auto --same-as $2
 elif [ "$1" = "notablet" ]; then
@@ -25,7 +25,7 @@ fi
 if [[ $1 = tablet* ]]; then
     xrandr --dpi 96
     xinput map-to-output $TABLET_STYLUS $SCREEN_TABLET
-    xinput set-prop $TABLET_STYLUS "Wacom Tablet Area" -283, 406, 32495, 32830
+    xinput set-prop $TABLET_STYLUS "Wacom Tablet Area" -546 278 58042 33183
 fi
 
 xrdb ~/.Xresources
