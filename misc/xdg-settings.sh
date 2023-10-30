4a138ddc396805d536fcaa93e20d88f09478b6b4
Jakob Ruckel <jakob@ruckel.de>
2023-10-30 01:40:53 +0100

    xdg: Set Firefox as default

--- misc/xdg-settings.sh
+++ misc/xdg-settings.sh
@@ -3,7 +3,7 @@
 # Set a bunch of X settings.
 # These settings persist, so this script only needs to be run once.
 
-xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop
+xdg-settings set default-web-browser firefox.desktop
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 xdg-mime default mpv.desktop video/x-matroska
 xdg-mime default mpv.desktop video/mp4
