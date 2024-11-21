84e71c3b5d8c48e967bb88cb982dfb820082a75f
Jakob Ruckel <jakob@ruckel.de>
2024-11-21 13:49:34 +0100

    i3: Set main screen as primary

--- .config/i3/custom.avail/000-xrandr-rechner.sh
+++ .config/i3/custom.avail/000-xrandr-rechner.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-SCREEN_LEFT=DP-0
+#SCREEN_LEFT=DP-0
 SCREEN_RIGHT=DP-4
 
-xrandr --output $SCREEN_LEFT --auto
-xrandr --output $SCREEN_RIGHT --auto --right-of $SCREEN_LEFT
+#xrandr --output $SCREEN_LEFT --auto
+xrandr --output $SCREEN_RIGHT --auto --primary #--right-of $SCREEN_LEFT
 
 ~/.fehbg
