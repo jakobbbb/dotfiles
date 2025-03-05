24a5eb1e5fdcbf2dadce718ecb3e1bb5a4ec85a8
Jakob Ruckel <jakob@ruckel.de>
2025-03-05 19:48:31 +0100

    i3/xrandr: Configure for new monitor setup

--- .config/i3/custom.avail/000-xrandr-rechner.sh
+++ .config/i3/custom.avail/000-xrandr-rechner.sh
@@ -2,10 +2,11 @@
 
 set -x
 
-#SCREEN_LEFT=DP-0
+SCREEN_LEFT=HDMI-0
 SCREEN_RIGHT=DP-4
 
-#xrandr --output $SCREEN_LEFT --auto
-xrandr --output $SCREEN_RIGHT --auto --primary #--right-of $SCREEN_LEFT
+xrandr --output $SCREEN_LEFT --auto --scale 2x2 --pos 0x0
+xrandr --output $SCREEN_RIGHT --primary --right-of $SCREEN_LEFT 
+xrandr --output $SCREEN_RIGHT --pos 3840x0
 
 ~/.fehbg
