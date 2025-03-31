96d48d7a5fa836870a6ad3cc10a010f12b340d74
Jakob Ruckel <jakob@ruckel.de>
2025-03-31 14:49:56 +0200

    i3/xrandr: Upgrade displays

--- .config/i3/custom.avail/000-xrandr-rechner.sh
+++ .config/i3/custom.avail/000-xrandr-rechner.sh
@@ -2,11 +2,10 @@
 
 set -x
 
-SCREEN_LEFT=DP-0
+SCREEN_LEFT=DP-2
 SCREEN_RIGHT=DP-4
 
-xrandr --output $SCREEN_LEFT --mode 2560x1440 --scale 1.5x1.5 --pos 0x0
-xrandr --output $SCREEN_RIGHT --mode 3840x2160 --primary --right-of $SCREEN_LEFT 
-xrandr --output $SCREEN_RIGHT --pos 3840x0
+xrandr --output $SCREEN_LEFT --mode 3840x2160
+xrandr --output $SCREEN_RIGHT --mode 3840x2160 --primary --right-of $SCREEN_LEFT
 
 ~/.fehbg
