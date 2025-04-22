34ecf0924684df896b8c98fbcc931044e0e82154
Jakob Ruckel <jakob@ruckel.de>
2025-04-22 11:48:53 +0200

    xrandr: Set resolution using auto

--- .config/i3/custom.avail/000-xrandr-rechner.sh
+++ .config/i3/custom.avail/000-xrandr-rechner.sh
@@ -5,7 +5,8 @@ set -x
 SCREEN_LEFT=DP-2
 SCREEN_RIGHT=DP-4
 
-xrandr --output $SCREEN_LEFT --mode 3840x2160
-xrandr --output $SCREEN_RIGHT --mode 3840x2160 --primary --right-of $SCREEN_LEFT
+xrandr --output $SCREEN_LEFT --auto
+xrandr --output $SCREEN_RIGHT --auto
+xrandr --output $SCREEN_RIGHT --primary --right-of $SCREEN_LEFT
 
 ~/.fehbg
